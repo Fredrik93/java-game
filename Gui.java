3ece7e98a58fbcd57e3ab8763398e2f7e4ef90ab
Fredrik Ullman <fredrikullman93@gmail.com>
2020-08-12 09:19:20 +0200

    intial commit

---
+++ Gui.java
@@ -0,0 +1,12 @@
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class Gui {
+    
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("The game GUI !");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500,500);
+        frame.setVisible(true);
+    }
+}
---
+++ Player.java
@@ -0,0 +1,15 @@
+import java.awt.*;
+
+public class Player {
+    String name;
+
+    public  Player(String name){
+        this.name = name;
+
+    }
+
+    public void paint(Graphics g){
+
+    }
+
+}
