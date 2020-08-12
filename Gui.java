85e715a2a9a8dc0bcf4d6e3fee013f55d62e52e8
Fredrik Ullman <fredrikullman93@gmail.com>
2020-08-12 14:10:52 +0200

    add main

--- Gui.java
+++ Gui.java
@@ -1,12 +1,16 @@
-import javax.swing.JButton;
+import java.awt.Color;
+
 import javax.swing.JFrame;
 
 public class Gui {
     
-    public static void main(String[] args) {
+    public Gui(){
+    
         JFrame frame = new JFrame("The game GUI !");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().setBackground(Color.gray);
         frame.setSize(500,500);
+        frame.setLocation(700, 300);
         frame.getContentPane().add(new Player());
         frame.setVisible(true);
     }
--- Player.java
+++ Player.java
@@ -5,9 +5,8 @@ import javax.swing.JComponent;
 public class Player extends JComponent{
 
     public void paint(Graphics g){
+        g.setColor(Color.green);
         g.fillRect(50, 50, 50, 50);
-        g.setColor(Color.orange);
-        
     }
 
 }
