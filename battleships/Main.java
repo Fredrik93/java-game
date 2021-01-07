2c1b46ace3c1acfaca6e356a2a653057b1936fef
Fredrik Ullman <fredrikullman93@gmail.com>
2021-01-07 10:04:32 +0100

    add battleship

--- battleships/Main.java
+++ battleships/Main.java
@@ -3,6 +3,7 @@ package battleships;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("WOrking");
+        System.out.println("battleships");
+
     }
 }
