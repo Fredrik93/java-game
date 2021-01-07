f2894b851ff74b51c27212f208da78eba209bbc5
Fredrik Ullman <fredrikullman93@gmail.com>
2021-01-07 14:07:23 +0100

    add scan

--- battleships/Main.java
+++ battleships/Main.java
@@ -1,9 +1,15 @@
 package battleships;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("battleships");
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("enter something");
+        String input = scan.nextLine();
+        System.out.println("your input was: " + input);
 
     }
 }
