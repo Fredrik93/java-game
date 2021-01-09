39d15676a07e956577fa0d1409a2923604fcf28b
Fredrik Ullman <fredrikullman93@gmail.com>
2021-01-09 07:50:31 +0100

    swap array to arraylist

--- battleships/Main.java
+++ battleships/Main.java
@@ -1,23 +1,27 @@
 package battleships;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        ArrayList<String> boats = new ArrayList<>();
+        boats.add("Submarine");
+        boats.add("Yellow Submarine");
+        System.out.println(boats);
         int x = 0;
         while (x != 9) {
-            System.out.println("Shoot at the boat, pick 0 - 4, or press 9 to exit");
+            System.out.println("Shoot at the boat, pick 0 - 8, or press 9 to exit");
             Scanner scn = new Scanner(System.in);
             int userInput = scn.nextInt();
             if (userInput == 9) {
                 x = 9;
             }
-            String array[];
-            array = new String[5];
-            array[0] = "Destroyer";
-            array[3] = "Commander";
-            if (array[userInput] != null) {
-                System.out.println("Yay you found the " + array[userInput].toString());
+            if (boats.isEmpty() == false) {
+                System.out.println("Yay you found the " + boats.get(userInput));
+            } else if (userInput == 9) {
+                System.out.println("********");
+                System.out.println("bye!");
             } else {
                 System.out.println("Oh dang its empty try again");
             }
