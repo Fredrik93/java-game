b538aa2fafbc8faad379ff72c07ce34e11765f05
Fredrik Ullman <fredrikullman93@gmail.com>
2021-01-10 06:05:42 +0100

    add new class

---
+++ battleships/GameHelper.java
@@ -0,0 +1,14 @@
+package battleships;
+
+import java.util.Scanner;
+
+public class GameHelper {
+
+    public static void Runner() {
+        System.out.println("Shoot at the boat, pick 0 - 8, or press 9 to exit");
+        Scanner scn = new Scanner(System.in);
+        int userInput = scn.nextInt();
+
+    }
+
+}
--- battleships/Main.java
+++ battleships/Main.java
@@ -4,17 +4,14 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
+        GameHelper game = new GameHelper();
         Battleship ship1 = new Battleship(1, "Commander");
         ArrayList<Battleship> boats = new ArrayList<>();
         boats.add(ship1);
         System.out.println(boats);
-        System.out.println("Shoot at the boat, pick 0 - 8, or press 9 to exit");
-        Scanner scn = new Scanner(System.in);
-        int userInput = scn.nextInt();
+        game.Runner();
 
-        if (boats.isEmpty() == false) {
-            System.out.println("Yay you found the " + boats.get(userInput));
-        }
     }
 }
